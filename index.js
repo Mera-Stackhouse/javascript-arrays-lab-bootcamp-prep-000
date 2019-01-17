258894a97e2bd4f0c237a9686b54945d55f7c53d
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 03:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveLastKitten() {
   return kittens
 }
 
-function destrivelyRemoveFirstKitten() {
+function destructivelyRemoveFirstKitten() {
   kittens.shift()
   return kittens
 }

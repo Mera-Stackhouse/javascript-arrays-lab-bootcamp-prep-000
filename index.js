fc82f2b018bac5a8dd3978878f27b2fc29badb80
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 03:07:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,8 +35,11 @@ function prependKitten(name) {
 function removeLastKitten() {
   console.log(kittens)
   var newKittens = kittens
+  console.log(kittens)
   newKittens.pop()
+  console.log(kittens)
   return newKittens
+  console.log(kittens)
   
   //return kittens.slice(0, kittens.length - 1)
 }
